cbf97a6f7518177ce4f59b2a8b7fc46593b47a6a
ashley taylor <ashleytaylor14@gmail.com>
2024-03-11 16:16:55 +0000

    chore: add DevStack

---
+++ infrastructure/src/env/DevStack.ts
@@ -0,0 +1,15 @@
+import { Construct } from 'constructs'
+import { Stack, StackProps } from 'aws-cdk-lib'
+
+import { Compute } from './constructs/compute'
+
+export default class DevStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props)
+
+    new Compute(this, 'Compute', {
+      env: 'dev',
+      project: 'saas'
+    })
+  }
+}
